c94c9c28e0c91c0a8e0483c1a4970eebb3046c24
Stavros Kalapothas <stavros.kalapothas@ext.ec.europa.eu>
2018-06-29 11:08:53 +0200

    add path to wsgi script

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
+if path not in sys.path:
+    sys.path.append(path)
+    
 import sys
-from myapp import app as mypythonapp
+from myapp import app as application
 
 if __name__ == '__main__':
     application.run( host='0.0.0.0' )
